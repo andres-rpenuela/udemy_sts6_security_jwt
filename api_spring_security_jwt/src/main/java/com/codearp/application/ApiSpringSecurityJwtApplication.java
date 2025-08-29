57e90471d1217ced7b74882c745ea0d06ac2121a
Andrés Ruiz Peñuela <andresruizpenuela@gmail.com>
2025-08-29 17:38:01 +0200

    [sts6] se crea el proyecto de spring con spring scurity + perfiles

---
+++ api_spring_security_jwt/src/main/java/com/codearp/application/ApiSpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.codearp.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringSecurityJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSpringSecurityJwtApplication.class, args);
+    }
+
+}
---
+++ api_spring_security_jwt/src/test/java/com/codearp/application/ApiSpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codearp.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringSecurityJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
